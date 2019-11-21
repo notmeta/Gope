25c445f02091fcaa5340cbaef547c8ce41db68ae
Corey <cpotts97@live.co.uk>
2019-11-21 18:25:24 +0000

    Code restructuring

--- main.go
+++ main.go
@@ -10,22 +10,41 @@ import (
 	"syscall"
 )
 
-func main() {
+var Scheduler = cron.New()
 
-	config, err := LoadConfig("test.toml")
+func main() {
+	defer Scheduler.Stop()
+
+	config, err := LoadConfig("jobs.toml")
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	RegisterTasks(config)
+
+	if len(config.Tasks) > 0 {
+		Scheduler.Start()
+	} else {
+		log.Println("No tasks to register, holding off starting the Scheduler")
+	}
+
+	// Wait for a CTRL-C
+	log.Printf(`Now running. Press CTRL-C to exit.`)
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+}
+
+func RegisterTasks(config *TaskConfig) {
 	successfulRegisters := 0
 	failedRegisters := 0
-	c := cron.New()
 
 	for name := range config.Tasks {
 		task := config.Tasks[name]
 
-		id, err := c.AddFunc(task.Interval, func() {
+		id, err := Scheduler.AddFunc(task.Interval, func() {
 			task.Run()
 		})
 
@@ -38,21 +57,6 @@ func main() {
 		}
 	}
 
-	if len(config.Tasks) > 0 {
-		c.Start()
-	} else {
-		log.Println("No tasks to register, holding off starting the scheduler")
-	}
-
 	log.Printf("%d task(s) registered.\n", successfulRegisters)
 	log.Printf("%d task(s) failed to register.\n", failedRegisters)
-
-	// Wait for a CTRL-C
-	log.Printf(`Now running. Press CTRL-C to exit.`)
-	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
-	<-sc
-
-	c.Stop()
-
 }
